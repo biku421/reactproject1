a6fc3698d33a1402f69fddb373b427199ce3965e
biku421 <pandeybivek@gmail.com>
2024-01-11 23:23:01 +0545

    docs is complete

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import Card from './card'
 function Foreground() {
+  const data =[
+  {desc:"My name is bivek pandey",filesize:".9mb", close:false,
+   tag:{isOpen: true,tagTitle:"Download Now",tagColor:"green"},
+  },
+  {desc:"My name is bivek pandey",filesize:".9mb", close:false,
+   tag:{isOpen: true,tagTitle:"Download Now",tagColor:"green"},
+  },
+  {desc:"My name is bivek pandey",filesize:".9mb", close:false,
+   tag:{isOpen: true,tagTitle:"Download Now",tagColor:"green"},
+  },
+  ];
   return (
-    <div className='fixed top-0 left-0  z-[3] w-full h-full'>  
-    <div>      </div>
-    
-       </div>
+    <div className='fixed top-0 left-0  z-[3] w-full h-full flex gap-5 flex-wrap p-10'>  
+    { data.map((item,index)=>(
+<Card data={item}/>
+    ))}
+    </div>
   )
 }
 
